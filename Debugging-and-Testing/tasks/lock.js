8b22322c4151de4f764d4d62fff5b2949ed3dc33
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-06-03 11:11:50 +0300

    Create task to send ethers

--- Debugging-and-Testing/tasks/lock.js
+++ Debugging-and-Testing/tasks/lock.js
@@ -10,3 +10,22 @@ task("accounts", "Prints the list of accounts")
       console.log(account.address);
     }
   });
+
+task("send", "Send ether to account")
+  .addParam("acc", "account")
+  .setAction(async (taskArgs, hre) => {
+    const account = (await hre.ethers.getSigners())[0];
+
+    const balance = await hre.ethers.provider.getBalance(account.address);
+    console.log(hre.ethers.utils.formatEther(balance));
+
+    const tx = await account.sendTransaction({
+      to: taskArgs.acc,
+      value: hre.ethers.utils.parseEther("1.0"),
+    });
+
+    await tx.wait();
+
+    const newBalance = await hre.ethers.provider.getBalance(account.address);
+    console.log(hre.ethers.utils.formatEther(newBalance));
+  });
