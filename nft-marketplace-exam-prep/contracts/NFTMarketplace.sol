2a0d16712b1b76b521a868abe1f71ed566a23811
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-06-04 23:10:22 +0300

    Create purchase and claim functionalities

--- nft-marketplace-exam-prep/contracts/NFTMarketplace.sol
+++ nft-marketplace-exam-prep/contracts/NFTMarketplace.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "./NFT.sol";
 
-contract NFTMarketplace is NFT {
+contract NFTMarketplace is NFT, Ownable {
     struct Sale {
         address seller;
         uint256 price;
@@ -11,13 +13,13 @@ contract NFTMarketplace is NFT {
 
     // collection => id => price
     mapping(address => mapping(uint256 => Sale)) public nftsForSale;
+    mapping(address => uint256) profits;
 
     function listNFTForSale(
         address collection,
         uint256 id,
         uint256 price
-    ) external {
-        require(_msgSender() == ownerOf(id), "Only owner can list NFT");
+    ) external onlyOwner {
         require(
             nftsForSale[collection][id].seller == address(0),
             "NFT is already listed"
@@ -28,5 +30,36 @@ contract NFTMarketplace is NFT {
 
         transferFrom(_msgSender(), address(this), id);
         approve(address(this), id);
+    }
+
+    function purchaseNFT(
+        address collection,
+        uint256 id,
+        address to
+    ) external payable {
+        Sale memory sale = nftsForSale[collection][id];
+        address seller = sale.seller;
+
+        require(seller != address(0), "NFT is not listed");
+        require(seller != _msgSender(), "You cannot buy your own NFT");
+        require(sale.price == msg.value, "Invalid price");
+
+        profits[seller] += msg.value;
+
+        delete nftsForSale[collection][id];
+
+        safeTransferFrom(address(this), _msgSender(), id);
+        approve(to, id);
+    }
+
+    function claimProfit() external onlyOwner {
+        uint256 profit = profits[_msgSender()];
+        require(profit != 0, "No profit to claim");
+
+        profits[_msgSender()] = 0;
+
+        (bool success, ) = payable(_msgSender()).call{value: profit}("");
+
+        require(success, "Failed to claim profit");
     }
 }
