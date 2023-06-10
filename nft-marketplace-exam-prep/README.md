ab5d655110d93eb236fee38b3ade60625f133d88
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-06-10 16:57:56 +0300

    Deploy and verify at sepolia

--- nft-marketplace-exam-prep/README.md
+++ nft-marketplace-exam-prep/README.md
@@ -1,5 +1,8 @@
 # Sample Hardhat Project
 
+## Depolyed and verify
+0xC34725129270336F443a3583fAbBcE2334BaAa7a
+
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
 Try running some of the following tasks:
--- nft-marketplace-exam-prep/hardhat.config.js
+++ nft-marketplace-exam-prep/hardhat.config.js
@@ -1,8 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 require("./tasks");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
